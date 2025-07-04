94cf889d7d0ae337d3ff3496926bc4d594887e71
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-07-04 17:38:05 +0900

    ✨ feat: 엔티티 설계 (Report, StudySession)
    - PeriodType Enum 추가
    - 연관관계 설정

--- src/main/java/com/poco/poco_backend/domain/member/entity/Member.java
+++ src/main/java/com/poco/poco_backend/domain/member/entity/Member.java
@@ -1,9 +1,13 @@
 package com.poco.poco_backend.domain.member.entity;
 
+import com.poco.poco_backend.domain.report.entity.Report;
+import com.poco.poco_backend.domain.studySession.entity.StudySession;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -28,5 +32,11 @@ public class Member {
 
     @Column(name = "email")
     private String email;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<StudySession> studySessions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Report> reports = new ArrayList<>();
 
 }
--- src/main/java/com/poco/poco_backend/domain/report/entity/Report.java
+++ src/main/java/com/poco/poco_backend/domain/report/entity/Report.java
@@ -1,7 +1,32 @@
 package com.poco.poco_backend.domain.report.entity;
 
-import jakarta.persistence.Entity;
+import com.poco.poco_backend.common.enums.PeriodType;
+import com.poco.poco_backend.domain.member.entity.Member;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Report {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PeriodType periodType;
+
+    private LocalDate baseDate;
+    private Double avgFocusScore;
+    private LocalDateTime focusPeakTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "report", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ReportSession> reportSessions = new ArrayList<>();
 }
