56ab3ed82eba649eb7d031849080d0b55e2eab86
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-07-10 05:35:41 +0900

    ♻️ refactor: 리포트 기간을 1/3/7일로 수정

--- src/main/java/com/poco/poco_backend/common/enums/PeriodType.java
+++ src/main/java/com/poco/poco_backend/common/enums/PeriodType.java
@@ -1,5 +1,17 @@
 package com.poco.poco_backend.common.enums;
 
 public enum PeriodType {
-    DAILY, WEEKLY, MONTHLY
+    DAY_1(1),
+    DAY_3(3),
+    DAY_7(7);
+
+    private final int days;
+
+    PeriodType(int days) {
+        this.days = days;
+    }
+
+    public int getDays() {
+        return days;
+    }
 }
