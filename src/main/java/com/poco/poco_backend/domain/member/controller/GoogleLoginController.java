504f41750b78eee467438bab8b794cb0e51a11e0
hanmin <rlemdhs2747@naver.com>
2025-07-09 20:42:15 +0900

    컨트롤러, 리포지토리에 관련 로직 추가

--- src/main/java/com/poco/poco_backend/domain/member/controller/GoogleLoginController.java
+++ src/main/java/com/poco/poco_backend/domain/member/controller/GoogleLoginController.java
@@ -3,11 +3,17 @@ package com.poco.poco_backend.domain.member.controller;
 import com.poco.poco_backend.domain.member.dto.request.CodeDTO;
 import com.poco.poco_backend.domain.member.dto.request.GoogleMemberDTO;
 import com.poco.poco_backend.domain.member.entity.Member;
+import com.poco.poco_backend.domain.member.repository.TokenRepository;
 import com.poco.poco_backend.domain.member.service.GoogleLoginService;
 import com.poco.poco_backend.global.CustomResponse;
 import com.poco.poco_backend.global.security.jwt.JwtDTO;
+import com.poco.poco_backend.global.security.jwt.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -23,11 +29,14 @@ public class GoogleLoginController {
 
     private final GoogleLoginService googleLoginService;
 
-    @Operation(summary = "구글 로그인 인가 및 토큰 발급"
+    private final JwtUtil jwtUtil;
+    private final TokenRepository tokenRepository;
+
+    @Operation(summary = "구글 회원가입 및 로그인"
             , description = """
-            클라이언트로부터 GET으로 전달받은 인가 코드를 이용하여 구글로부터 accessToken을 발급 받습니다.\n
-            발급 받은 accessToken에서 사용자의 정보를 추출한 뒤, 서버 accessToken, refreshToken을 발급한 뒤,\n
-            accessToken을 클라이언트로 전송합니다.            
+            클라이언트로부터 POST로 전달받은 인가 코드를 이용하여 구글로부터 accessToken을 발급 받습니다.\n
+            발급 받은 accessToken에서 사용자의 정보를 추출한 뒤, 서버 accessToken, refreshToken을 발급한 뒤, 
+            클라이언트로 전송합니다.            
             """
     )
     @PostMapping("/google")
@@ -50,5 +59,26 @@ public class GoogleLoginController {
         log.info("[ Google Login Controller ] 토큰을 재발급 합니다.");
 
         return CustomResponse.onSuccess(googleLoginService.reissueToken(jwtDto));
+    }
+
+    @Transactional
+    @SecurityRequirement(name = "JWT TOKEN")
+    @Operation(summary = "로그아웃", description = "로그아웃 api입니다.")
+    @DeleteMapping("/logout")
+    public CustomResponse<?> logout(HttpServletRequest request) throws SignatureException {
+
+        String accessToken = jwtUtil.resolveAccessToken(request);
+
+        log.info("[ Google Login Controller ] 토큰으로부터 이메일을 추출합니다..");
+
+        String email = jwtUtil.getEmail(accessToken);
+
+        log.info("[ Google Login Controller ] 로그아웃을 진행합니다.");
+
+        tokenRepository.deleteByEmail(email);
+
+        log.info("[ Google Login Controller ] 로그아웃이 완료 되었습니다..");
+
+        return CustomResponse.onSuccess("로그아웃 되었습니다.");
     }
 }
