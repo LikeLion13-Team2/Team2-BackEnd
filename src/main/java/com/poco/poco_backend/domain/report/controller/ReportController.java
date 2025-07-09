798c642e56042fd27fe94fc388a07b1c926712a3
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-07-10 06:00:22 +0900

    🚀 chore: Swagger 설명 추가

--- src/main/java/com/poco/poco_backend/domain/report/controller/ReportController.java
+++ src/main/java/com/poco/poco_backend/domain/report/controller/ReportController.java
@@ -9,6 +9,8 @@ import com.poco.poco_backend.domain.report.dto.response.ReportResponseDTO;
 import com.poco.poco_backend.domain.report.service.query.ReportQueryService;
 import com.poco.poco_backend.global.CustomResponse;
 import com.poco.poco_backend.global.security.auth.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
+@Tag(name = "Report", description = "리포트 관련 API by 박현빈")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/reports")
@@ -24,6 +27,11 @@ public class ReportController {
     private final ReportQueryService reportQueryService;
     private final MemberRepository memberRepository;
 
+    @Operation(summary = "리포트 조회",
+            description = """
+    기간 타입(1일/3일/7일)과 기준 날짜를 기반으로 학습 리포트를 조회합니다.\n
+    baseDate 예시: 2025-07-10
+    """)
     @GetMapping
     public CustomResponse<ReportResponseDTO.ReportDetailResponseDTO> getReport(
             @RequestParam PeriodType periodType,
